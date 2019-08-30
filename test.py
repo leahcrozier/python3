462002287e51dbf45ab0066d8786df91fb4a4d75
leahcrozier <37397477+leahcrozier@users.noreply.github.com>
2019-08-30 12:26:05 +0200

    Create test.py
    versie 1, test

---
+++ test.py
@@ -0,0 +1,13 @@
+test = {"een": "A", "twee": "B", "drie": "C"}
+
+input1 = input("Kies een getal tussen een en drie ");
+
+if (input1 == "een"):
+    print(test["een"])
+elif (input1 == "twee"):
+    print (test["twee"])
+elif (input1 == "drie"):
+    print(test["drie"]
+else:
+    print("geen een, twee of drie ingevuld")    
+
