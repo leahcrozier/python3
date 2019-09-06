91dbc308e61f6a9dc2af4f4cb7322afe6544cef8
leahcrozier <37397477+leahcrozier@users.noreply.github.com>
2019-09-06 11:27:05 +0200

    +tafel van 5

--- functies.py
+++ functies.py
@@ -5,3 +5,17 @@ def helloworld():
         print("Hello World!");
 
 helloworld()
+
+print("================================================")
+
+def tafelvanvijf():
+    print(str(1*5))
+    print(str(2*5))
+    print(str(3*5))
+    print(str(4*5))
+    print(str(5*5))
+    print(str(6*5))
+
+
+
+tafelvanvijf()
