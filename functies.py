dbdecd5faab273cf8692cebd03595139e60e7873
leahcrozier <37397477+leahcrozier@users.noreply.github.com>
2019-09-10 15:32:00 +0200

    +is_priemgetal

--- functies.py
+++ functies.py
@@ -65,4 +65,15 @@ reverse_string("ok, this is epic.")
 
 print("=========================8============================")
     
+def is_priemgetal(x):
+    if x >1:
+        for i in range(2,x):
+            if (x % i) == 0:
+                print ("False")
+            else:
+                print("True")
+
+    else:
+        print("False")
     
+is_priemgetal(3)
