134483c92f5ee2cf4a2f6220938bd9e666582b77
leahcrozier <37397477+leahcrozier@users.noreply.github.com>
2019-09-26 10:21:30 +0200

    Update eindopdracht.py

--- eindopdracht.py
+++ eindopdracht.py
@@ -13,17 +13,18 @@ def print_functie():
 7. Klik op x om te stoppen.''')
     print("                       ")
 
-def n1():
-    print(assortiment)        
+def print_assortiment():
+    print("                            ")
+    print("Dit zit in ons assortiment ",assortiment)        
         
-def n2():
+def voeg_product_toe():
     producttoevoeg = input("Geef de naam van een product: ")
     bedragtoevoeg = int(input("Geef het bedrag van het product: "))
     assortiment[producttoevoeg] = bedragtoevoeg
     print("Dit zit er nu in je boodschappenmandje: ")
-    n1()
+    print_assortiment()
     
-def n3():
+def verwijder_product():
     print(assortiment)
     productverwijder = input("Geef de naam van een product die je wil verwijderen: ")
 
@@ -32,81 +33,87 @@ def n3():
         print(assortiment)
         return assortiment
     else:
-        print("Dat is geen product. ")
+        print("Dat is geen product ")
 
-def n4():
+def wijzig_product():
     print(assortiment)
     productwijzigen = input("Geef de naam van een product die je wil wijzigen: ")
     
     if productwijzigen in assortiment:
-        wijzig = input("Waarnaar ga je de product wijzigen? ")
-        prijs = int(input("Wat wordt de prijs? "))
+        wijzig = input("Waarnaar ga je de product wijzigen?: ")
+        prijs = int(input("Wat wordt de prijs?: "))
         
         assortiment[wijzig] = assortiment.pop(productwijzigen)
         assortiment[wijzig] = prijs
         print (assortiment)
     else:
-        print("ok")
-def n5():
+        print("Dit is geen product. ")
+    
+def toevoegen_boodschappenmand():
     kaas = True
     mandje = {}
+    prijs = 0
     while kaas ==  True:
         print("Dit is wat we te koop hebben: ")
         print(assortiment)
-        input0 = input("Wil je (nog) iets kopen? (ja / nee) : ")
+        print("                   ")
+        input0 = input("Wil je (nog) iets kopen?(ja/nee): ")
+        print("                   ")
         if input0 == "ja":
             input1 = input("Wat wil je kopen? ")
             if input1 in mandje:
                 l = assortiment[input1]
                 r = int(input("Hoeveel wil je ervan kopen? "))
                 w = l*r
-                #x = assortiment.get(input1)
-                #print(x)
                 mandje[input1] = int(w)
                 print(mandje)
+            elif (input1 != assortiment):
+                print("Dit zit niet in ons assortiment. ")
+                print("           ")
             else:
                 x = assortiment.get(input1)
-                print(x)
+                prijs = prijs + x
+                print("                   ")
+                print("Zoveel kost dit product: ",prijs)
+                print("                   ")
                 mandje[input1] = int(x)
-                print(mandje)
         elif input0 == "nee":
             kaas = False
         else:
             print("Typ aub alleen 'ja' of 'nee'")
-    prijs = mandje.values()
-    print(prijs)
+    
+    print("                   ")
+    print(mandje, "Dit zit er nu in uw mandje. ")
+    print("Je boodschappen kosten", prijs, "euro. ")
         
-    print(mandje, "Dit zit er nu in uw mandje")
-        
-def n6():
+def sla_op():
     print(assortiment)
     opslaan = open("producten.txt","w")
     opslaan.write(str(assortiment))
     opslaan.close
     print("Alle producten zijn opgeslagen.")
     
-def n7():
+def afsluiten():
     print("doei")
 while slose == False:
     print_functie()
     inpurt = input("Kies een optie: ")
         
     if (inpurt == "1"):
-        n1()
+        print_assortiment()
     elif (inpurt == "2"):
-        n2()
+        voeg_product_toe()
     elif (inpurt == "3"):
-        n3()
+        verwijder_product()
     elif (inpurt == "4"):
-        n4()
+        wijzig_product()
     elif (inpurt == "5"):
-        n5()
+        toevoegen_boodschappenmand()
     elif (inpurt == "6"):
-        n6()
+        sla_op()
     elif (inpurt == "x"):
-        n7()
+        afsluiten()
         slose = True
-
             
 
 
